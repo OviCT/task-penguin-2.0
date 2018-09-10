3f5b95031996255d535ca6879771735d1a0d7c98
Cătăramă Ovidiu <catarama.ovidiu66@gmail.com>
2018-09-10 21:55:19 +0300

    update menu.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<img src="logos/logo2.png" width=875 height=575 alt="logo">
 	</div> 
 	<div class="sticky">
-		<?php include 'menu/menu.php'; ?>
+		<?php include 'menu.php'; ?>
 	</div>
 </body>
 </html>
