900097f84cd233be4f852ab604c098c707335c5f
Cătăramă Ovidiu <catarama.ovidiu66@gmail.com>
2018-09-10 21:04:42 +0300

    add index.php

---
+++ add-view.php
@@ -0,0 +1,18 @@
+
+<?php include 'style/import-style.php' ?>
+<?php include 'menu.php' ?>
+
+<div class="container">
+	<form action="crud/create.php" method="post">
+		<div class="form-group">
+			<label for="title">Task title</label>
+			<input type="text" name="title" class="form-control" id="title" placeholder="Enter title">
+		</div>
+		<div class="form-group">
+			<label for="description">Task description</label>
+			<textarea class="form-control" name="content" id="description" aria-describedby="emailHelp" placeholder="Describe the task"></textarea>
+		</div>
+
+	<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<body>
+	<div class="container">
+		<img src="logo.png" width=500 height=300>
+	</div>
+	<div >
+			<?php include 'menu.php' ?>
+	</div>
+
+
+</body>
+</html>
