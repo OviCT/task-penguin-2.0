529a3deb557f0928e29ee286c3a24b1f2bce33d6
IsacLucian <40325553+IsacLucian@users.noreply.github.com>
2018-09-10 22:37:09 +0300

    update add-view

--- add-view/add-view.php
+++ add-view/add-view.php
@@ -1,6 +1,6 @@
 
-<?php include '../style/import-style.php'; ?>
-<?php include '../menu.php'; ?>
+<?php include 'style/import-style.php' ?>
+<?php include 'menu.php' ?>
 
 <div class="container">
 	<form action="crud/create.php" method="post">
@@ -12,26 +12,7 @@
 			<label for="description">Task description</label>
 			<textarea class="form-control" name="content" id="description" aria-describedby="emailHelp" placeholder="Describe the task"></textarea>
 		</div>
-	    <div class="row">
-	        <div class='col-sm-6'>
-	            <div class="form-group">
-	                <div class='input-group date' id='datetimepicker3'>
-	                    <input type='text' class="form-control" placeholder="Pick the time of the task"/>
-	                    <span class="input-group-addon">
-	                        <span class="glyphicon glyphicon-time"></span>
-	                    </span>
-	                </div>
-	            </div>
-	        </div>
-	        <script type="text/javascript">
-	            $(function () {
-	                $('#datetimepicker3').datetimepicker({
-	                    format: 'LT'
-	                });
-	            });
-	        </script>
-	    </div>
-	    <button type="submit" class="btn btn-primary">Submit</button>
 
+	<button type="submit" name="submit" class="btn btn-primary">Submit</button>
 	</form>
 </div>
